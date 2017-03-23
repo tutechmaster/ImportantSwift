4ba32d26d02e6bd9742d0101e4f115b1978505c5
Tuuu <tu@techmaster.vn>
2017-03-23 12:48:17 +0700

    benchmark

--- ClassAndStruct/ClassAndStruct.playground/Contents.swift
+++ ClassAndStruct/ClassAndStruct.playground/Contents.swift
@@ -3,32 +3,105 @@
 import UIKit
 
 class House {
-    var colorName: String
-    init(colorName: String) {
+    var colorName = [String]()
+    init(colorName: [String]) {
         self.colorName = colorName
+    }
+    init() {
+        colorName = [String]()
+    }
+    func run(n: Int)
+    {
+        for index in 0 ... n
+        {
+            let a = index
+        }
     }
 }
 
-var aClass = House(colorName: "Red")
-var bClass = aClass // aClass and bClass now reference the same instance!
-bClass.colorName = "Green"
+//var aClass = House(colorName: "Red")
+//var bClass = aClass // aClass and bClass now reference the same instance!
+//bClass.colorName = "Green"
 
-print(aClass.colorName) // "Green"
-print(bClass.colorName) // "Green"
+//print(aClass.colorName) // "Green"
+//print(bClass.colorName) // "Green"
 
 struct HouseStruct {
-    var colorName: String
-    init(colorName: String) {
+    var colorName = [String]()
+    init(colorName: [String]) {
         self.colorName = colorName
+    }
+    init() {
+        colorName = [String]()
+    }
+    func run(n: Int)
+    {
+        for index in 0 ... n
+        {
+            let a = index
+        }
     }
 }
 
-var aStruct = HouseStruct(colorName: "Red")
-var bStruct = aStruct // aStruct and bStruct are two structs with the same value!
-bStruct.colorName = "Green"
+//var aStruct = HouseStruct(colorName: "Red")
+//var bStruct = aStruct // aStruct and bStruct are two structs with the same value!
+//bStruct.colorName = "Green"
+//
+//print(aStruct.colorName) //"Red"
+//print(bStruct.colorName) //"Green"
+var containtClass = NSMutableArray()
+var containtStruct = Array<HouseStruct>()
+var aClass = House()
+var aStruct = HouseStruct()
+func createClasses()
+{
+    for index in 0...500
+    {
+        var aClass = House()
+        aClass.run(n: 50)
+    }
+}
+func createStructs()
+{
+    for index in 0...500
+    {
+        var aStruct = HouseStruct()
+        aStruct.run(n: 50)
+    }
+}
+//createClasses()
+//createStructs()
 
-print(aStruct.colorName) //"Red"
-print(bStruct.colorName) //"Green"
+func loadStruct()
+{
+    for index in 0...500
+    {
+        aStruct.colorName[index]
+    }
+}
+func loadClass()
+{
+    for index in 0...500
+    {
+        aClass.colorName[index]
+    }
+}
+
+
+
+
+let startTimeStruct = CFAbsoluteTimeGetCurrent()
+createClasses()
+let timeElapsedStruct = CFAbsoluteTimeGetCurrent() - startTimeStruct
+print("createStructs: \(timeElapsedStruct)")
+
+
+let startTime = CFAbsoluteTimeGetCurrent()
+createClasses()
+let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+print("createClasses: \(timeElapsed)")
+
+
 
 
 
